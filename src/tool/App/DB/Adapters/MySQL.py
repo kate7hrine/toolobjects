160f56c4604a6a76f947b9451b8b3a99c50fe630
v00d00mag1c <234468996+v00d00mag1c@users.noreply.github.com>
2026-02-10 22:22:16 +0300

    feat: sqlalchemy adapter improvements

--- src/tool/App/DB/Adapters/MySQL.py
+++ src/tool/App/DB/Adapters/MySQL.py
@@ -1,4 +1,20 @@
 from App.DB.Adapters.SQLAlchemy import SQLAlchemy
+from App.DB.Query.Condition import Condition
+from pydantic import Field
 
 class MySQL(SQLAlchemy):
-    protocol_name = 'mysql'
+    # Not tested
+
+    protocol_name = 'mysql+pymysql'
+    username: str = Field()
+    password: str = Field(default = None)
+    host: str = Field()
+    port: int = Field()
+    db_name: str = Field(default = None)
+    charset: str = Field(default = 'utf8mb4')
+
+    def _before_init_models(self):
+        from sqlalchemy_utils import database_exists, create_database
+
+        if not database_exists(self._engine.url): 
+            create_database(self._engine.url)
